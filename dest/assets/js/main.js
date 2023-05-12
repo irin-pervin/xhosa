ec8a61e7a27b0b82ad517531b242748ffada64b9
irin-pervin <irinpervin156@gmail.com>
2023-05-13 01:15:18 +0600

    swiper slider add complitely done

--- dest/assets/js/main.js
+++ dest/assets/js/main.js
@@ -1,2 +1,60 @@
 console.log('all Js here')
 
+/*
+** With Slick Slider Plugin : https://github.com/marvinhuebner/slick-animation
+** And Slick Animation Plugin : https://github.com/marvinhuebner/slick-animation
+*/
+
+// Init slick slider + animation
+// $('.slider').slick({
+//     autoplay: true,
+//     speed: 800,
+//     lazyLoad: 'progressive',
+//     arrows: true,
+//     dots: true,
+//       prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+//       nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+//   }).slickAnimation();
+  
+  
+  
+//   $('.slick-nav').on('click touch', function(e) {
+  
+//       e.preventDefault();
+  
+//       let arrow = $(this);
+  
+//       if(!arrow.hasClass('animate')) {
+//           arrow.addClass('animate');
+//           setTimeout(() => {
+//               arrow.removeClass('animate');
+//           }, 1600);
+//       }
+  
+//   });
+
+
+// swiper 
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev"
+    // },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar"
+    }
+  });
+  
